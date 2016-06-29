08b880e3da1e38e2143c0afeb73de13203113de0
karthickneev <karthick.nagarajan@neevtech.com>
2016-06-29 15:59:34 +0530

    Custom payment method magento 2

---
+++ Payment/Model/Testpay.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Explorer\Payment\Model;
+
+class Testpay extends \Magento\Payment\Model\Method\AbstractMethod
+{
+
+    protected $_code = 'testpay';
+
+    protected $_isOffline = true;
+
+
+}
